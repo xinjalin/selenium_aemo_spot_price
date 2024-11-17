1fecd871a58995b51361eed435be104d57567ddc
xinjalin <xinjalin@gmail.com>
2024-11-17 23:56:51 +1030

    project set up
    added req.txt and setup .py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='selenium_aemo_spot_price',
+    version='0.1',
+    packages=['src', 'src.core', 'src.tests', 'src.utils'],
+    url='',
+    license='',
+    author='Christopher Osborn',
+    author_email='xinjalin@gmail.com',
+    description='webscraper'
+)
